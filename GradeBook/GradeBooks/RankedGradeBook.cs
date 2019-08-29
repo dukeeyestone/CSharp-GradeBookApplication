9b49d941ad9ce9154c93cf669884a769a1724759
dukeeyestone <54574227+dukeeyestone@users.noreply.github.com>
2019-08-29 14:33:43 +0200

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,7 @@
-﻿namespace GradeBook.GradeBooks
+﻿using System;
+using System.Linq;
+
+namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
@@ -7,5 +10,34 @@
             Type = Enums.GradeBookType.Ranked;
 
          }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw InvalidOperationException("Zu wenig Studenten");
+            }
+
+            var treshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[treshold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[treshold * 2 - 1] <= averageGrade)
+                return 'B';
+            else if (grades[treshold * 3 - 1] <= averageGrade)
+                return 'C';
+            else if (grades[treshold * 4 - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
+        }
+
+
+
+        private Exception InvalidOperationException(string v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
