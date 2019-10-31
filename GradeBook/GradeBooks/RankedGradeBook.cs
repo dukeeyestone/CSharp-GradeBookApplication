fca3372dfb5a5a1b21bd51cbbbd856546263a247
dukeeyestone <54574227+dukeeyestone@users.noreply.github.com>
2019-10-31 15:24:40 +0100

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -33,6 +33,16 @@ namespace GradeBook.GradeBooks
                 return 'F';
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+             
+            base.CalculateStatistics();
+        }
 
 
         private Exception InvalidOperationException(string v)
